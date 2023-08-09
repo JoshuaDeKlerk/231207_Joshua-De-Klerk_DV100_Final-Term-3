efb583905ab085c52c6df3c71595bd62aeac9cbc
JoshuaDeKlerk <231207@virtualwindow.co.za>
2023-08-09 21:29:32 +0200

    Homepage and trips

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// When the document loads
+$(document).ready(function(){
+
+    console.log("Hello World" );
+
+    // Home Page
+
+    // Hide the description text from the plant card
+    $("#WelcomeMessage").hide();
+})
